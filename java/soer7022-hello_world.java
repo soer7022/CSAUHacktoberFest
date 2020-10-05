4b375bdd4ba0c41d3f9d831e6d0841a08968a838
Søren Eriksen <soeren.skov.eriksen@lego.com>
2020-10-05 09:30:05 +0200

    add initial hello world files in java and python

---
+++ java/soer7022-hello_world.java
@@ -0,0 +1,7 @@
+// LANGUAGE: Java
+// AUTHOR: Søren Eriksen
+// GITHUB: https://github.com/soer7022
+
+public static void main() {
+    System.out.println("Hello, World!");
+}
---
+++ python/soer7022-hello_world.py
@@ -0,0 +1,5 @@
+# LANGUAGE: Python
+# AUTHOR: Søren Eriksen
+# GITHUB: https://github.com/soer7022
+
+print("Hello, World!")
