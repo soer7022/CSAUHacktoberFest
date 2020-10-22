d0e4290ea1cccc297071323bbe26564d71450f40
Acecto <73301916+Acecto@users.noreply.github.com>
2020-10-22 18:08:52 +0200

    Add HelloWorld

---
+++ java/Acecto-HelloWorld.java
@@ -0,0 +1,8 @@
+
+public class HelloWorld
+{
+    // instansvariabler - erstat eksemplet herunder med dine egne variabler
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
