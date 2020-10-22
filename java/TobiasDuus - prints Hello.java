43ce1a12b0fe372a60120510ce131872a4406433
TobiasDuus <73301826+TobiasDuus@users.noreply.github.com>
2020-10-22 18:08:46 +0200

    Add Mega cool hello code

---
+++ java/TobiasDuus - prints Hello.java
@@ -0,0 +1,14 @@
+
+/**
+ * Lav en beskrivelse af klassen HelloWorld her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class HelloWorld
+{
+    
+    public static void main() {
+        System.out.println("Hello");
+    }
+}
