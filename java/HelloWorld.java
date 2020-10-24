87b5a04321897be7dd049d3cfc3ca78ce96a1103
sebiwnl <69086256+sebiwnl@users.noreply.github.com>
2020-10-24 20:28:21 +0200

    Prints Hello World

---
+++ java/HelloWorld.java
@@ -0,0 +1,13 @@
+
+/**
+ * Lav en beskrivelse af klassen HelloWorld her.
+ * 
+ * @author (dit navn her)
+ * @version (versions nummer eller dato her)
+ */
+public class HelloWorld
+{
+    public static void main() {
+        System.out.println("Hello, World!");
+    }
+}
