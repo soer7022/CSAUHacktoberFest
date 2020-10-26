81f7e676c7d36f05aeaeaab8cffcbe617148bd18
Crowton <Crowton@users.noreply.github.com>
2020-10-26 15:05:12 +0100

    Code cleanup

--- java/Ak-Food.java
+++ java/Ak-Food.java
@@ -5,34 +5,34 @@
  * @author Anne Kirstine Overgaard
  * @version (a version number or a date)
  */
-public class Food implements Comparable<Food>
-{
+public class Food implements Comparable<Food> {
+    
     private String name;
     private int weight;
     private int price;
 
-    public Food(String name, int weight, int price)
-    {
-
+    public Food(String name, int weight, int price) {
         this.name = name;
         this.price=price;
         this.weight = weight;
     }
 
-    public String toString(){
+    public String toString() {
         return weight + " gram " + name + " for " + price + " DKK";
     }
 
-    public int getWeight(){
-        return weight;}
+    public int getWeight() {
+        return weight;
+    }
 
     public int getPrice(){
-        return price;}
+        return price;
+    }
 
     public int compareTo(Food other){
         if(!name.equals(other.name)){
             return name.compareTo(other.name);
         }
-        return  other.weight - weight;
+        return other.weight - weight;
     }
 }
